1aad954fc4c2a508828afe00866cf60110a65cdc
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-02 22:00:37 +0100

    pseudocode for printing the day of the week

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,21 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return """
+                   If X is 1
+                    Output Monday is 1
+                   Else If X is 2
+                    Output Tuesday is 2
+                   Else If X is 3
+                    Output Wednesday is 3
+                   Else If X is 4
+                    Output Thursday is 4
+                   Else If X is 5
+                    Output Friday is 5
+                   Else If X is 6
+                    Output Saturday is 6
+                   Else  X is 7
+                    Output Sunday is 7
+                   Else X is 8
+                     Do Noting
+        """
