46f17896ae799182d9d8e011465dd91241ee6553
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-04 23:47:49 +0100

    Pseudocode for withdrawing money from ATM

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,13 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+                           Input Card
+                           Provide Security Code (Pin Code)
+                           Select/type amount of money for withdrawing
+                           Press Enter
+                           Choose option wheather to print receipt or not                           
+                           Collect Card and/or Receipte
+                           Collect Money
+
+                """
