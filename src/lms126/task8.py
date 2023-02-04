8524643c651264407cadf379a3d7dcc182e4cb3d
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-04 23:37:54 +0100

    Pseudocode for checking multiples of 3 and 5

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,18 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return """
+                          Input X
+                        
+                              If X divided by 3 without remainder 
+                                 Output Foo
+                              If X divided by 5 without remainder 
+                                 Output Bar
+                              If X divided by 15 without remainder 
+                                 Output FooBar
+                              If X divided by 15 with remainder 
+                                 Output Qix
+                              
+
+               """
+
