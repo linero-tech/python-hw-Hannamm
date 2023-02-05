71110a4b32275d63c3dc8744480f015b438f74ca
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-05 20:07:26 +0100

    Pseudocode for converting temperatures from Celcius to Fahrenheit  and vise versa

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+                               Celcius = (°F - 32) x 5/9
+                               Fahrenheit = (°C x 9/5) + 32
+                               
+                               Input temperature in Celcius = 20°C 
+                               Output temperature in Fahrenheit will be (20°C x 9/5) + 32 = 68°F 
+                               
+                               Input temperature in Fahrenheit = 60°C 
+                               Output temperature in Celcius will be (60°F - 32) x 5/9 = 15.6°C 
+
+                    """
+
