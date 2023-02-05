fd4223122a376b6255a0225a55d5f930c2437dec
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-05 19:47:08 +0100

    Pseudocode for creating an email account

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """
+                               Open hotmail/gmail or other mail interfaces
+                               Input First name and last name
+                               Input User name 
+                               Input Password more than six characters long
+                               Click on next/accept button
+                               If Password length is less than six characters
+                                  Output Password should be atleast six charcter length
+                               Input Password more than six characters long
+                               Click on next/accept button
+                               Output Your email account is created Sucessfully!!                      
+                               
+                               
+            """
