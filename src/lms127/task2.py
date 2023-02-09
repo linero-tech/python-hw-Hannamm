30559d33b70b0b4fd0f3ad84278fee738bbd4071
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-09 13:15:02 +0100

    Print a sentence

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -2,10 +2,11 @@ from to_do import TODO
 
 
 def task2():
+    name = "Hanna"
+    country = "Ethiopia"
+    month = "August"
+    print(f"Hello {name}, you are from {country} and you were born in {month}")
 
-              name = "Hanna"
-              country = "Ethiopia"
-              month = "August"
-              return ("Hello " + name + ", you are from " + country + " and you were born in " + month )
 
-print(task2())
+if __name__ == "__main__":
+    task2()
