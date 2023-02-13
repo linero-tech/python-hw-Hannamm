08253feb0f42773f9974f325d3f03df0009d3d47
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-13 22:53:06 +0100

    Count the number of times character appears in sentence

--- src/lms127/task8.py
+++ src/lms127/task8.py
@@ -2,6 +2,8 @@ from to_do import TODO
 
 
 def task8(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = sentence.count("I")
+    return result
+if __name__ == "__main__":
+    print("result is", task8("I code in KOTLIN", "I"))
+
