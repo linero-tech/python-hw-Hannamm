35ef4ea73a7b74cde7aa16eca7d0b8ba08108203
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-13 21:49:16 +0100

    Swap the value of a and b

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -1,11 +1,15 @@
 from to_do import TODO
 
-
 def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
 
-    TODO("Your solutions goes in here.")
+    temp = a
+    a = b
+    b = temp
 
-    # Do not erase or change the below statement
+        # Do not erase or change the below statement
     return a, b
+if __name__ == "__main__":
+    print(f"a is {task5(1,2)[0]} \nb is {task5(1,2)[1]}" )
+
