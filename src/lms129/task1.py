a21a27d50e1562a1cdfa7c1a9fc4422b64674584
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-02-15 09:33:34 +0100

    Add the numbers between a and b

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -1,7 +1,23 @@
-from to_do import TODO
-
 
 def task1(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+    if a < b:
+        result = sum(range(a, b+1))
+        return result
+
+
+if __name__ == "__main__":
+    print("result is", task1(1, 5))
+
+def task_2(a, b):
+    result = 0
+    if a < b:
+        for i in range(a, b+1):
+            result += i
+        else:
+            result = 0
+    return result
+
+
+if __name__ == "__main__":
+    print("result is", task_2(3, 3))
