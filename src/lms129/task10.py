2d6bfc005576f55913999aed96c7d6345d99264d
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 22:26:14 +0100

    Create a password checker program that determines if the password is valid or not.

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -2,23 +2,23 @@ import re
 
 
 def task10(password):
-    state = True
+    result = True
 
     if len(password) < 6 or len(password) > 10:
-        state = False
+        result = False
     elif not re.search("[a-z]", password):
-        state = False
+        result = False
 
     elif not re.search("[0-9]", password):
-        state = False
+        result = False
 
     elif not re.search("[A-Z]", password):
-        state = False
+        result = False
 
     elif not re.search("[$#@]", password):
-        state = False
+        result = False
 
-    if state:
+    if result:
         result = "Valid Password"
     else:
         result = "Not Valid Password"
