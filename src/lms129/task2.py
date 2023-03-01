8cd33a2dc9c31a622edc895089f084b9e1f16625
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 15:03:01 +0100

    Update task2.py

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -1,17 +1,19 @@
-
 def task2(number):
-
+    result = True
     if number > 1:
         for i in range(2, number):
             if (number % i) == 0:
                 result = False
                 break
-        else:
-         result = True
-    else:
-        result = False
+
     return result
 
 
+# A prime number is as a positive integer >=1
+# with only two factors: itself and one
 if __name__ == "__main__":
-    print("result  is ", task2(5))
+    print("result  is ", task2(5))  # True
+    print("result  is ", task2(3))  # True
+    print("result  is ", task2(7))  # True
+    print("result  is ", task2(4))  # False
+    print("result  is ", task2(26))  # False
