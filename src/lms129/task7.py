1b7114ec6a7da4cf120d920e7cccd80764c8b575
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 16:22:16 +0100

    Find the result raising a to the power of b using loops

--- src/lms129/task7.py
+++ src/lms129/task7.py
@@ -1,7 +1,17 @@
-from to_do import TODO
-
-
 def task7(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 1
+
+    if a == 0 and b == 0:
+        result = 1
+    else:
+        for i in range(b):
+            result *= a
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task7(2, 3))
+    # print("result is ", task7(4, 4))
+    # print("result is ", task7(5, 3))
+    # print("result is ", task7(2, 4))
