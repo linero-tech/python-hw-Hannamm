a123192f848df7869a6abd0050af6ee1efcf679a
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 22:21:35 +0100

    Create a password checker program that determines if the password is valid or not.

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -1,7 +1,33 @@
-from to_do import TODO
+import re
 
 
 def task10(password):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    state = True
+
+    if len(password) < 6 or len(password) > 10:
+        state = False
+    elif not re.search("[a-z]", password):
+        state = False
+
+    elif not re.search("[0-9]", password):
+        state = False
+
+    elif not re.search("[A-Z]", password):
+        state = False
+
+    elif not re.search("[$#@]", password):
+        state = False
+
+    if state:
+        result = "Valid Password"
+    else:
+        result = "Not Valid Password"
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task10("Animate2#"))  # Valid Password
+    print(task10("Animate211#"))  # Not Valid Password
+    print(task10("ani21@"))  # Not Valid Password
+    print(task10("Ani222"))  # Not Valid Password
