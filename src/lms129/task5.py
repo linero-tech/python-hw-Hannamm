19d9947bf5392986cf1b04e80914ea8ba8417429
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 16:10:21 +0100

    Count the number of characters in the variable sentence using loops

--- src/lms129/task5.py
+++ src/lms129/task5.py
@@ -2,6 +2,12 @@ from to_do import TODO
 
 
 def task5(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+
+    for _ in sentence:
+        result += 1
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task5("I love GBG"))
