4bf5884ca57b2c13c5104d914b53de01abdf5ccd
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 15:22:07 +0100

    Calculate the factorial of the number

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -1,10 +1,11 @@
-
 def task3(number):
     result = 1
-    for i in range(1, number+1):
+    for i in range(1, number + 1):
         result *= i
     return result
 
 
 if __name__ == "__main__":
     print("result  is ", task3(5))
+    print("result  is ", task3(4))  # 24
+    print("result  is ", task3(6))  # 4720
