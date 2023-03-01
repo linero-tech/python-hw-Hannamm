c201efa9af18117fd6c4d626b48bcd6fbfafc7bd
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-01 18:12:09 +0100

    Convert the given temperature to the opposite unit

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -1,7 +1,19 @@
-from to_do import TODO
-
 
 def task9(temperature):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+    if temperature[-1].upper() == "C":
+        degree = int(temperature[:-1])
+        result = round(degree * 9 / 5 + 32)
+        result = str(result) + "F"
+    elif temperature[-1].upper() == "F":
+        degree = int(temperature[:-1])
+        result = round((degree - 32) * 5 / 9)
+        result = str(result) + "C"
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task9("-30C"))
+    print("result is ", task9("50F"))
+    # print("result is ", task9("40c"))
+    # print("result is ", task9("-65f"))
