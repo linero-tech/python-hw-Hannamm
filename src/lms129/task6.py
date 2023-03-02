df115947bd99edcf5bcd0929ce8eb9b8093a48ad
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-02 16:12:42 +0100

    Find the reversed version of the number

--- src/lms129/task6.py
+++ src/lms129/task6.py
@@ -1,7 +1,20 @@
-from to_do import TODO
-
 
 def task6(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+    while number > 0:
+        digit = number % 10  # Get the last digit of the number
+        result = result * 10 + digit  # Add the digit to the result in reverse order
+        number //= 10  # Remove the last digit from the number
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task6(678))
+    print("result is ", task6(1))
+    # print("result is ", task6(11))
+    # print("result is ", task6(5))
+    # print("result is ", task6(21))
+    # print("result is ", task6(1234))
+    # print("result is ", task6(23))
+
