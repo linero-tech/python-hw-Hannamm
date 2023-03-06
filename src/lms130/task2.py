feec20d0baa9a8c5995e4b7f43ae0d05bbf9623f
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 14:11:41 +0100

    Add all the elements that are located at an even index.

--- src/lms130/task2.py
+++ src/lms130/task2.py
@@ -2,6 +2,22 @@ from to_do import TODO
 
 
 def task2(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+
+    if items:  # check if items is not empty
+        for i in range(0, len(items), 2):
+            result += items[i]
+    else:
+        result = 0
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task2([]))  # 0
+    print("result is ", task2([1, 2, 3, 4]))  # 4
+    print("result is ", task2([1, 2]))  # 1
+    print("result is ", task2([5, 7, 9]))  # 14
+    print("result is ", task2([1, 1, -2, 3, 4]))  # 3
+    print("result is ", task2([0, 1, 2, 3, 4, 5]))  # 6
+    print("result is ", task2([8, 9, 0, 6, 3, 6, 8, 4, 6, 456, 3, 5, 3, 7, 4, 5566, 234, 1, 234, 35646, 234]))  # 737
