144bda2661dcd780e277b96eea5e7aeb55594487
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 15:40:10 +0100

    Change to uppercase every character located in an even position

--- src/lms130/task6.py
+++ src/lms130/task6.py
@@ -1,7 +1,17 @@
-from to_do import TODO
-
-
 def task6(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = ""
+
+    for i in range(len(sentence)):
+        if i % 2 == 0:
+            result += sentence[i].upper()
+        else:
+            result += sentence[i]
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task6("I like Gothenburg"))  # I LiKe GOtHeNbUrG
+    print("result is ", task6("I LOVE SWEDEN"))  # I LOVE SWEDEN
+    print("result is ", task6("abcde"))  # AbCdE
+    print("result is ", task6("abcdef abcde abcde"))  # AbCdEf aBcDe aBcDe
