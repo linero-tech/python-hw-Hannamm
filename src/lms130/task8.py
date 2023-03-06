bf9b6b0c6aad9a511a29b01f991bf646c9491cc7
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 16:07:50 +0100

    Identify the three elements with the highest value

--- src/lms130/task8.py
+++ src/lms130/task8.py
@@ -1,7 +1,20 @@
-from to_do import TODO
-
-
 def task8(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    # Sort the list in ascending order
+    items.sort()
+
+    # Get the last three elements
+    result = items[-3:]
+
+    # Reverse the order to get ascending order
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task8([60, 9, 7, 10]))  # [9, 10, 60]
+    print("result is ", task8([1, 2, 3, 4, 5]))  # [3, 4, 5]
+    print("result is ", task8([4, 3, 2, 1]))  # [2, 3, 4]
+    print("result is ", task8( [-1, -2, -3, -4]))  # [-3, -2, -1]
+    print("result is ", task8([]))  # 0
