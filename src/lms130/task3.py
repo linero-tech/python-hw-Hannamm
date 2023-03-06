81ce3cb8cf6c8f58ba70d793b2df4a1a471d0b39
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 14:29:04 +0100

    Identify all the elements that appear more than once

--- src/lms130/task3.py
+++ src/lms130/task3.py
@@ -1,7 +1,16 @@
-from to_do import TODO
-
-
 def task3(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+    for i in items:
+        if items.count(i) > 1 and i not in result:
+            result.append(i)
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task3([]))  # []
+    print("result is ", task3([1, 1, 1, 2, 2, 3]))  # [1,2]
+    print("result is ", task3([0, 0, 0, 0]))  # [0]
+    print("result is ", task3([-9, -9, 9, 1, 1, -9, 9, 3, 2, 2]))  # [-9, 9, 1, 2]
+    print("result is ", task3([1, 1, -2, 3, 4]))  # [1]
+    print("result is ", task3([5, 6, 5, 6, 7, 8, 7]))  # [5, 6, 7]
