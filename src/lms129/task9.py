dbae11760a8f18d9917d23ddf2b73af3322e620e
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 10:58:36 +0100

    Convert the given temperature to the opposite unit

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -1,19 +1,37 @@
 
 def task9(temperature):
-    result = 0
-    if temperature[-1].upper() == "C":
-        degree = int(temperature[:-1])
-        result = round(degree * 9 / 5 + 32)
-        result = str(result) + "F"
-    elif temperature[-1].upper() == "F":
-        degree = int(temperature[:-1])
-        result = round((degree - 32) * 5 / 9)
-        result = str(result) + "C"
+    # if temperature[-1] == "C" or temperature[-1] == "c":
+    #     # Convert from Celsius to Fahrenheit
+    #     result = float(temperature[:1]) * 9 / 5 + 32
+    #     result = str(result) + "F"
+    # elif temperature[-1] == "F" or temperature[-1] == "f":
+    #     # Convert from Fahrenheit to Celsius
+    #     result = (float(temperature[:1]) - 32) * 5 / 9
+    #     result = str(result) + "C"
+    # else:
+    #     result = "Invalid input"
+    import math
+
+    if temperature[-1] == "C" or temperature[-1] == "c":
+        # Convert from Celsius to Fahrenheit
+        result = float(temperature[:-1]) * 9 / 5 + 32
+        result = str(math.trunc(result)) + "F"
+    elif temperature[-1] == "F" or temperature[-1] == "f":
+        # Convert from Fahrenheit to Celsius
+        result = (float(temperature[:-1]) - 32) * 5 / 9
+        result = str(math.trunc(result)) + "C"
+    else:
+        result = "Invalid input."
+
     return result
 
 
 if __name__ == "__main__":
-    print("result is ", task9("-30C"))
-    print("result is ", task9("50F"))
-    # print("result is ", task9("40c"))
-    # print("result is ", task9("-65f"))
+    print("result is ", task9("-30C"))  # -22F
+    print("result is ", task9("50f"))  # 10C
+    print("result is ", task9("1C"))  # 33F
+    print("result is ", task9("0F"))  # 17C
+    print("result is ", task9("-90F"))  # -67
+    print("result is ", task9("20f"))  # -6C
+    print("result is ", task9("1"))  # invalid
+    print("result is ", task9("0h"))  # invalid
