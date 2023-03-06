c8764e17f668e999dad6fc952c9305a476cac733
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 15:54:13 +0100

    Find the element with the smallest value

--- src/lms130/task7.py
+++ src/lms130/task7.py
@@ -1,7 +1,19 @@
-from to_do import TODO
-
 
 def task7(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+
+    if len(items) > 0:
+        result = items[0]
+    for item in items:
+        if item < result:
+            result = item
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task7([10, 5, 11]))  # 5
+    print("result is ", task7([1, 2, 3, 4, 5]))  # 1
+    print("result is ", task7([3, 2, 1, 4, 5]))  # 1
+    print("result is ", task7([-100, -90, -10, 1, -2, 3]))  # -100
+    print("result is ", task7([]))  # 0
