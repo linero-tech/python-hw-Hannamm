37a9b2281a868417be3a6c57763b601364d5eebe
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 13:50:14 +0100

    Select a random element from it

--- src/lms130/task1.py
+++ src/lms130/task1.py
@@ -1,7 +1,17 @@
-from to_do import TODO
-
-
 def task1(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    import random
+
+    if items:
+        result = random.choice(items)
+    else:
+        result = 0
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task1([]))  # True
+    print("result is ", task1([1, 2, 3, 4, 5, 6]))  # True
+    print("result is ", task1([-1, -2, -3, -4, -5, -6]))
+    print("result is ", task1([1, 2, 3, 4, 5]))
+    print("result is ", task1([1, 2, 3, 4, 5]))
