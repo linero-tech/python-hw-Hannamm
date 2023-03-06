e294d8d150564be2baf808a827f32bdbc1963225
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-06 14:44:45 +0100

    Identify all the elements in items which are a factor of factor

--- src/lms130/task4.py
+++ src/lms130/task4.py
@@ -1,7 +1,15 @@
-from to_do import TODO
-
 
 def task4(items, factor):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = list(set([item for item in items if item % factor == 0]))
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task4([], 2))  # []
+    print("result is ", task4([1, 2, 4, 5, 6], 2))  # [2, 4, 6]
+    print("result is ", task4([0, 0, 0, 0], 2))  # [0]
+    print("result is ", task4([-9, -9, 9, 1, 1, -9, 9, 3, 2, 2], 3))  # [2]
+    print("result is ", task4([1, 1, -2, 3, 4], 4))  # [4, -2]
+    print("result is ", task4([5, 6, 5, 6, 7, 8, 7], 5))  # [8, 6]
