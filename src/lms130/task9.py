165475821b95e61f880d29bbe35c68dba7271020
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-07 09:27:22 +0100

    Create a new list by multiplying each element of items by the previous element

--- src/lms130/task9.py
+++ src/lms130/task9.py
@@ -1,7 +1,21 @@
-from to_do import TODO
-
-
 def task9(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    if items:
+        result.append(items[0] ** 2)  # multiply the first element by itself
+        for i in range(1, len(items)):
+            result.append(items[i] * items[i - 1])  # multiply each element by the previous element
+    else:
+        result = []
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is ", task9([5, 2, 3, 4]))  # [25, 10, 6, 12]
+    print("result is ", task9([9, 1, 2, 3, 6]))  # [81, 9, 2, 6, 18]
+    print("result is ", task9([4]))  # [16]
+    print("result is ", task9([1, -2, 3, -4]))  # [1, -2, -6, -12]
+    print("result is ", task9([]))  # []
+    print("result is ", task9([0, 1, 0, 1]))  # [0, 0, 0, 0]
+    print("result is ", task9([1, 1, 1, 1]))  # [1, 1, 1, 1]
