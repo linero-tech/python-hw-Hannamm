63c9da5217ccebf13241d3878b7b2ef1af7dde16
HannaMorde <71565185+Hannamm@users.noreply.github.com>
2023-03-13 10:19:19 +0100

    It represents a list of people attending an event.

--- src/lms130/task11.py
+++ src/lms130/task11.py
@@ -9,9 +9,7 @@ def task11_1(guests):
 
 def task11_2(guests, condition):
     condition = "-K"
-    result = [guest for guest in guests if guest.endswith(condition)]
-
-    return result
+    return [guest for guest in guests if guest.endswith(condition)]
 
 
 if __name__ == "__main__":
@@ -26,11 +24,5 @@ if __name__ == "__main__":
         "Maëline-K",
     ]
 
-    # NOTE: Uncomment the code below when you are ready to test your answers
-    print(f"The event has a total of {task11_1(guest_list)} guests.")
-    print(
-        f"The attendees with condition '{situation}' are {task11_2(guest_list, situation)}"
-    )
     print("result is", task11_1(["Lia-K", "Mar-A", "Luz-K", "Ulf-V"]))
-    print("result is", task11_2(["Lia-K", "Mar-A", "Luz-K", "Ulf-V"], "-V"))
     print("result is", task11_2(["Lia-K", "Mar-A", "Luz-K", "Ulf-V"], "-V"))
